1bf21e97e5b4617cbb0129f35367393fe66d8db1
Nathan Laan <nathanlaan@gmail.com>
2013-04-28 22:17:13 -0600

    Adding models.

---
+++ KB.Web/Models/EntryModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KB.Web.Models
+{
+    public class EntryModel
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Contents { get; set; }
+        public List<string> TagList { get; set; }
+    }
+}
---
+++ KB.Web/Models/ResponseVoteModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KB.Web.Models
+{
+    public class ResponseVoteModel
+    {
+        public int ID { get; set; }
+        public int ResponseID { get; set; }
+        public int AuthorID { get; set; }
+        public int Score { get; set; }
+    }
+}
