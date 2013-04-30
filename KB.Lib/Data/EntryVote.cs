317367c3f35839272af48a548507517e6e5c1614
nathan laan <nathanlaan@gmail.com>
2013-04-29 22:00:59 -0600

    Added user entity.

--- KB.Lib/Data/EntryVote.cs
+++ KB.Lib/Data/EntryVote.cs
@@ -8,7 +8,7 @@ namespace KB.Lib.Data
     public class EntryVote : BaseEntity
     {
         public int EntryID { get; set; }
-        public int AuthorID { get; set; }
+        public int UserID { get; set; }
         public int Score { get; set; }
     }
 }
