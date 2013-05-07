ea9adac911c93c40beb6c1ba97b1bfa4129dcb1b
Nathan Laan <nathanlaan@gmail.com>
2013-05-07 15:29:38 -0600

    Added mail utility for sending SMTP mail.

---
+++ KB.Lib/Utility/MailUtility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+
+namespace KB.Lib.Utility
+{
+    public sealed class MailUtility
+    {
+
+        public void SendMail(string from, string to, string subject, string message, string smtpServer = "localhost")
+        {
+            try
+            {
+                MailMessage mailMessage = new MailMessage(from, to, subject, message);
+                SmtpClient smtpClient = new SmtpClient(smtpServer);
+                smtpClient.UseDefaultCredentials = true;
+                smtpClient.Send(mailMessage);
+            }
+            catch
+            {
+                //
+                // TODO: Logging
+                //
+            }
+        }
+
+    }
+}
